29c926f938b9e2126d6935a1f615d71aba33580f
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-12-25 21:39:28 +0600

    add domain code

--- internal/clean/authoralt/domain.go
+++ internal/clean/authoralt/domain.go
@@ -1,4 +1,4 @@
-package nonsplit
+package authoralt
 
 import (
 	"context"
@@ -15,29 +15,45 @@ type Author struct {
 // Repository defines the data persistance logic that needs to be implemented
 type Repository interface {
 	Create(ctx context.Context, a Author, args ...any) (string, error)
+
 	ReadMany(ctx context.Context, args ...any) ([]Author, error)
+
 	ReadOne(ctx context.Context, id string, args ...any) (Author, error)
+
 	Update(ctx context.Context, id string, a Author, args ...any) (Author, error)
+
 	Delete(ctx context.Context, id string, args ...any) (Author, error)
 }
 
 type Writer interface {
 	Create(ctx context.Context, a Author, args ...any) (string, error)
+	
+	Update(ctx context.Context, id string, a Author, args ...any) (Author, error)
+	
+	Delete(ctx context.Context, id string, args ...any) (Author, error)
+}
+
+type Reader interface {
+	ReadMany(ctx context.Context, args ...any) ([]Author, error)
+
+	ReadOne(ctx context.Context, id string, args ...any) (Author, error)
 }
 
 // RepositoryCQRS defines the data persistance logic that needs to be implemented
 type RepositoryCQRS interface {
-	Create(ctx context.Context, a Author, args ...any) (string, error)
-	ReadMany(ctx context.Context, args ...any) ([]Author, error)
-	ReadOne(ctx context.Context, id string, args ...any) (Author, error)
-	Delete(ctx context.Context, id string, args ...any) (Author, error)
+	Writer
+	Reader
 }
 
 // UseCase defines the business logic that needs to be implemented
 type UseCase interface {
 	Create(ctx context.Context, dto dto.CreateUserDTO, args ...any) (Author, error)
+	
 	ReadMany(ctx context.Context, args ...any) ([]Author, error)
+	
 	ReadOne(ctx context.Context, id string, args ...any) (Author, error)
+	
 	Update(ctx context.Context, id string, dto dto.UpdateUserDTO, args ...any) (Author, error)
+	
 	Delete(ctx context.Context, id string, args ...any) (Author, error)
 }
